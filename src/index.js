1dc0980eed51dc2a09d4760959d803f505f42976
lizethvela <angelica.velazquez7697@alumnos.udg.mx>
2021-04-28 14:53:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+function mycomponente(){
+  return <p> hola word </p> 
+}
+
+  
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
