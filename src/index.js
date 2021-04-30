108a85f6301cbb9cdfacd629e8bd61c4185bd7bb
lizethvela <angelica.velazquez7697@alumnos.udg.mx>
2021-04-30 15:52:08 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-function mycomponente(){
-  return <p> hola word </p> 
+function mycomponente() {
+  return <p> hola word </p>;
 }
 ReactDOM.render(<App />, document.getElementById("root"));
